f0e690ebf914cc251d0c431adbb04df5ac7cbcf7
Katkatkat333 <bkeywebstudio@gmail.com>
2022-01-30 14:40:39 +0200

    Add navigation with NavLinks and use react-router-dom v6

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,8 @@ import s from './Header.module.css';
 const Header = () => {
     return (
         <header className={s.header}>
-            <img className={s.logo} src="https://uploadstatic-sea.mihoyo.com/contentweb/20200324/2020032418381582458.png" alt=""/>
+            <img className={s.logo}
+                 src="https://uploadstatic-sea.mihoyo.com/contentweb/20200324/2020032418381582458.png" alt=""/>
         </header>
     )
 }
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,23 +1,28 @@
 import React from 'react';
 import s from './Nav.module.css';
+import {NavLink} from "react-router-dom";
 
 const Nav = () => {
+
+    let activeClassButton = ({isActive}) =>
+        isActive ? `${s.link} ${s.activeLink}` : s.link
+
     return (
         <nav className={s.nav}>
             <div className={s.item}>
-                <a href="" className={`${s.link} ${s.active}`}> Profile</a>
+                <NavLink to="/profile" className={activeClassButton}> Profile</NavLink>
             </div>
             <div className={s.item}>
-                <a href="" className={s.link}> Messages</a>
+                <NavLink to="/dialogs" className={activeClassButton}> Messages</NavLink>
             </div>
             <div className={s.item}>
-                <a href="" className={s.link}> News</a>
+                <NavLink to="/news" className={activeClassButton}> News</NavLink>
             </div>
             <div className={s.item}>
-                <a href="" className={s.link}> Music</a>
+                <NavLink to="/music" className={activeClassButton}> Music</NavLink>
             </div>
             <div className={s.item}>
-                <a href="" className={s.link}> Settings </a>
+                <NavLink to="/settings" className={activeClassButton}> Settings </NavLink>
             </div>
         </nav>
     )
