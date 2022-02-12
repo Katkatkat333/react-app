3a59cbbd3b0253693b81b8ae848a691eb9767285
Katkatkat333 <bkeywebstudio@gmail.com>
2022-02-12 17:04:50 +0200

    Create store object in state.js and refactoring

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,23 +3,34 @@ import s from './Dialogs.module.css';
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
 
-const Dialogs = ({state}) => {
+const Dialogs = ({state, addMessage, newMessageText, updateNewMessageText}) => {
 
     let dialogsElements = state.dialogsData.map(dialog => <DialogItem name={dialog.name} id={dialog.id}/>)
     let messagesElements = state.messagesData.map(message => <Message text={message.text} id={message.id}/>)
 
     let newMessageElement = React.createRef();
-    let addMessage = () => {
-        let text = newMessageElement.current.value;
-        alert(text)
+    let addNewMessage = () => {
+        addMessage();
+        updateNewMessageText('');
     }
+
+    let onMessageChange = () => {
+        let text = newMessageElement.current.value;
+        updateNewMessageText(text);
+    }
+
     return (
         <div className={s.dialogs}>
             <div className={s.messages}>
                 <div className={s.messagesElements}>{messagesElements}</div>
                 <div className={s.addMessage}>
-                    <textarea cols="60" rows="4" ref={newMessageElement} className={s.newMessage}></textarea>
-                    <button className={s.button} onClick={addMessage}>Send</button>
+                    <textarea cols="60" rows="4"
+                              ref={newMessageElement}
+                              className={s.newMessage}
+                              value={newMessageText}
+                              onChange={onMessageChange}
+                    />
+                    <button className={s.button} onClick={addNewMessage}>Send</button>
                 </div>
             </div>
             <div className={s.dialog_items}>
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,53 +1,75 @@
-import {rerenderEntireTree} from "../render";
+let store = {
+    _state: {
+        profile: {
+            postData: [
+                {id: 1, message: 'Hello, My name is Kate', likes: 6},
+                {id: 2, message: 'The London is a capital of Great Britain', likes: 100},
+                {id: 3, message: 'hello world', likes: 2}
+            ],
+            newPostText: 'KATERYSHKA'
+        },
+        dialogs: {
+            dialogsData: [
+                {id: 1, name: 'Kate'},
+                {id: 2, name: 'Dmytro'},
+                {id: 3, name: 'Bohdan'},
+                {id: 4, name: 'Julia'}
+            ],
+            messagesData: [
+                {id: 1, text: 'Hello'},
+                {id: 2, text: 'How are you? The London is a capital of Great Britain'},
+                {id: 3, text: 'Fine'},
+            ],
+            newMessageText: 'bla'
+        },
+        friends: {
+            friendsData: [
+                {id: 1, name: 'Anna', surname: 'Goroshko', avatar: 'https://avatanplus.com/files/effects/mid/5ed1143d9387817260b710a4.jpg'},
+                {id: 2, name: 'Julia', surname: 'Surkova', avatar: 'https://i.pinimg.com/736x/ab/01/df/ab01dfc62541394a1c094d09fe64ecfb.jpg'},
+                {id: 3, name: 'Anna', surname: 'Babko', avatar: 'https://img.freepik.com/free-photo/the-beautiful-girl-stands-near-walll-with-leaves_8353-5378.jpg'},
+            ]
+        }
+    },
+    getState() {
+        return this._state;
+    },
+    _callSubscriber() {
+        console.log('state changed');
+    },
+    addPost() {
+        let newPost = {
+            id: 5,
+            message: this._state.profile.newPostText,
+            likes: 0,
+        };
 
-let state = {
-    profile: {
-        postData: [
-            {id: 1, message: 'Hello, My name is Kate', likes: 6},
-            {id: 2, message: 'The London is a capital of Great Britain', likes: 100},
-            {id: 3, message: 'hello world', likes: 2}
-        ],
-        newPostText: 'KATERYSHKA'
+        this._state.profile.postData.push(newPost);
+        this._state.profile.newPostText = '';
+        this._callSubscriber(this._state);
     },
-    dialogs: {
-        dialogsData: [
-            {id: 1, name: 'Kate'},
-            {id: 2, name: 'Dmytro'},
-            {id: 3, name: 'Bohdan'},
-            {id: 4, name: 'Julia'}
-        ],
-        messagesData: [
-            {id: 1, text: 'Hello'},
-            {id: 2, text: 'How are you? The London is a capital of Great Britain'},
-            {id: 3, text: 'Fine'},
-        ]
+    updateNewPostText(newText) {
+        this._state.profile.newPostText = newText;
+        this._callSubscriber(this._state);
     },
-    friends: {
-        friendsData: [
-            {id: 1, name: 'Anna', surname: 'Goroshko', avatar: 'https://avatanplus.com/files/effects/mid/5ed1143d9387817260b710a4.jpg'},
-            {id: 2, name: 'Julia', surname: 'Surkova', avatar: 'https://i.pinimg.com/736x/ab/01/df/ab01dfc62541394a1c094d09fe64ecfb.jpg'},
-            {id: 3, name: 'Anna', surname: 'Babko', avatar: 'https://img.freepik.com/free-photo/the-beautiful-girl-stands-near-walll-with-leaves_8353-5378.jpg'},
-        ]
+    addMessage() {
+        debugger;
+        let newMessage = {
+            id: 100,
+            text: this._state.dialogs.newMessageText,
+        }
+        this._state.dialogs.messagesData.push(newMessage);
+        this._state.dialogs.newMessageText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewMessageText(newText) {
+        this._state.dialogs.newMessageText = newText;
+        this._callSubscriber(this._state);
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer;
     }
 }
 
-window.state = state;
+window.store = store
 
-export let addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.profile.newPostText,
-        likes: 0,
-    };
-
-    state.profile.postData.push(newPost);
-    state.profile.newPostText = '';
-    rerenderEntireTree(state);
-}
-
-export let updateNewPostText = (newText) => {
-    state.profile.newPostText = newText;
-    rerenderEntireTree(state);
-}
-
-export default state;
+export default store;
